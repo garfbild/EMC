2f84f5257ca209c486be428286da5af7198bac30
garfbild <30866252+garfbild@users.noreply.github.com>
2020-03-03 23:37:08 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from keras.datasets import mnist,cifar10
 from matplotlib import pyplot as plt
 import random
 from keras.models import Sequential, model_from_json
-from keras.layers import Dense, Activation, Conv2D, Flatten, MaxPooling2D
+from keras.layers import Dense, Activation, Conv2D, Flatten, MaxPooling2D, Dropout
 import numpy as np
 from keras.utils import to_categorical
 
@@ -57,10 +57,14 @@ def cnnMNIST():
                   loss='mean_squared_error',
                   metrics=['accuracy'])
     model.summary()
+    model.save("model.h5")
 
-    history = model.fit(x, y, epochs=10, batch_size=1024,verbose=0,validation_split = 0.05)
+
+
+
+    history = model.fit(x, y, epochs=10, batch_size=1024,verbose=2,validation_split = 0.05)
     print("done")
-    results = model.evaluate(x = xtest,y = ytest,verbose = 0,batch_size = 32)
+    results = model.evaluate(x = xtest,y = ytest,verbose = 1,batch_size = 32)
     print(model.metrics_names,results)
     print(history.history.keys())
     plt.plot(history.history['accuracy'])
@@ -76,13 +80,47 @@ def cnnMNIST():
 
 def cnnCIFAR():
     (x_train, y_train), (x_test, y_test) = cifar10.load_data()
-    x = np.reshape(x_train,(50000,32,32,3))
+    xtrain = np.reshape(x_train,(50000,32,32,3))
     xtest = np.reshape(x_test,(10000,32,32,3))
-    y = to_categorical(y_train)
+    ytrain = to_categorical(y_train)
     ytest = to_categorical(y_test)
+    plt.imshow(xtrain[random.randint(1,10000)], cmap='hsv')
+    plt.show()
 
     model = Sequential()
+    model.add(Conv2D(filters = 48, kernel_size = (3,3), strides=(1,1), data_format="channels_last", padding = 'same',activation = 'relu', input_shape = (32,32,3)))
+    model.add(Conv2D(filters = 48, kernel_size = (3,3), strides=(1,1), data_format="channels_last", padding = 'same',activation = 'relu'))
+    model.add(MaxPooling2D(pool_size = (2,2), padding = 'same', strides = 2))
+    model.add(Dropout(0.25))
+    model.add(Conv2D(filters = 96, kernel_size = (3,3), strides=(1,1), data_format="channels_last", padding = 'same',activation = 'relu', input_shape = (32,32,3)))
+    model.add(Conv2D(filters = 96, kernel_size = (3,3), strides=(1,1), data_format="channels_last", padding = 'same',activation = 'relu'))
+    model.add(MaxPooling2D(pool_size = (2,2), padding = 'same', strides = 2))
+    model.add(Dropout(0.25))
+    model.add(Conv2D(filters = 192, kernel_size = (3,3), strides=(1,1), data_format="channels_last", padding = 'same',activation = 'relu', input_shape = (32,32,3)))
+    model.add(Conv2D(filters = 192, kernel_size = (3,3), strides=(1,1), data_format="channels_last", padding = 'same',activation = 'relu'))
+    model.add(MaxPooling2D(pool_size = (2,2), padding = 'same', strides = 2))
+    model.add(Dropout(0.25))
+    model.add(Flatten())
+    model.add(Dense(512,activation = 'relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(256,activation = 'relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(10,activation = 'softmax'))
 
-
+    model.compile(optimizer='adam',
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+    model.summary()
+    history = model.fit(xtrain, ytrain, epochs=1, batch_size=800,verbose=1,validation_split = 0.05)
+    results = model.evaluate(x = xtest,y = ytest,verbose = 1,batch_size = 32)
+    print(model.metrics_names,results)
+    print(history.history.keys())
+    plt.plot(history.history['accuracy'])
+    plt.plot(history.history['val_accuracy'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.show()
+    model.save("cnn.h5")
 
 cnnMNIST()
